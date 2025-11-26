bb788f9415fc921b4532ea2e1701d0136dc974ad
renren <renrenda77@gmail.com>
2025-11-27 00:11:10 +0900

    Add more cases for plus command

--- test.bash
+++ test.bash
@@ -1,14 +1,28 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Ren Okazaki
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-  echo ${1}行目が違うよ
-  res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "$out" = 15 ] || ng "$LINENO"
+
+### 異常な入力（文字）
+out=$(echo あ | ./plus)
+[ $? = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+### 異常な入力（空）
+out=$(echo | ./plus)
+[ $? = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
 
